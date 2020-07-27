b7c95716401b151de4ae4b84b5861088000342aa
Jack Firth <jackhfirth@gmail.com>
2020-07-26 18:52:19 -0700

    Fill out template

--- info.rkt
+++ info.rkt
@@ -1,12 +1,12 @@
 #lang info
 
-(define collection "racket-package-template")
+(define collection "glass")
 
 (define scribblings
   (list (list "main.scrbl"
               (list 'multi-page)
               (list 'library)
-              "racket-package-template")))
+              "glass")))
 
 (define deps
   (list "base"))
--- main.scrbl
+++ main.scrbl
@@ -1,4 +1,4 @@
 #lang scribble/manual
 
-@title{Racket Package Template}
-@defmodule[racket-package-template]
+@title{Glass: Composable Optics}
+@defmodule[glass]
